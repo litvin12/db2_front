990a9de9e56374414ae71e60e43fa25af1141974
xpxcx <egorov11012005@gmail.com>
2025-04-24 23:00:12 +0500

    m

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { MainLayout } from './layouts/MainLayout'
 import { Home } from './components/pages/Home'
-import { Profile } from './components/pages/Profile'
+// import { Profile } from './components/pages/Profile'
 import { Registration } from './components/Registration/Index'
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <MainLayout /> }>
           <Route path='/' element={ <Home/> } />
-          <Route path='/profile' element={ <Profile /> } />
+          {/* <Route path='/profile' element={ <Profile /> } /> */}
           <Route path='/registration' element={ <Registration />} />
         </Route>
       </Routes>
